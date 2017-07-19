5ea9cca08a4d2ca746529327f685301c2ed031be
Kuan Jung Chiang <b02901092@ntu.edu.tw>
2017-07-20 03:15:11 +0800

    more sleep

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@ sudo bash ./centers/clean.sh
 sudo python ./centers/server_scan.py &
 sleep 2
 sudo ./centers/sensorCenter &
+sleep 2
 sudo ./bluez-5.43/attrib/gatttool -S &
+sleep 2
 sudo bash ./echo/main.sh &
