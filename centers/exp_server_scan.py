f647f6d94d1df064e7e568f59397106902804a6f
Kuan Jung Chiang <b02901092@ntu.edu.tw>
2017-07-20 14:48:10 +0800

    200

--- centers/exp_server_scan.py
+++ centers/exp_server_scan.py
@@ -71,7 +71,7 @@ while True:
             print "Received broadcast from neighbor: ", tmpType, tmpPower
             if tmpType == type and tmpPower > neighborPowerMax:
                 neighborPowerMax = tmpPower
-        if x > 4 and power > neighborPowerMax and ( tmpType == type or x - CON_SIG_TIMER > 3 ) and tmpType == type - 1:
+        if x > 4 and power > neighborPowerMax and ( tmpType == type or x - CON_SIG_TIMER > 1 ) and tmpType == type - 1:
             if address not in s:
                 if(address[0] != '0' and address[0] != 'B'):
                     print "User Device"
